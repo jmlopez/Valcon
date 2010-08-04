82849591ef577dfe53b4d0096056fb9c1b385c21
Josh Arnold <rncodepoet@gmail.com>
2010-08-04 10:27:02 -0500

    Cleaning up the project / removing unused types

--- Source/Valcon/Registration/Graph/RuleDef.cs
+++ Source/Valcon/Registration/Graph/RuleDef.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Valcon.Registration.Graph
 {
@@ -7,7 +6,6 @@ namespace Valcon.Registration.Graph
     {
         public RuleDef()
         {
-            Dependencies = new List<IDependency>();
             Name = Guid.NewGuid().ToString();
         }
 
@@ -19,46 +17,10 @@ namespace Valcon.Registration.Graph
 
         public string Name { get; set; }
         public Type Type { get; set; }
-        public object Value { get; set; }
-        public IList<IDependency> Dependencies { get; set; }
-
-        public RuleDef Child(Type dependencyType, Type actualType)
-        {
-            var dependency = new RuleDef(actualType);
-            Dependencies.Add(new ConfiguredDependency
-            {
-                DependencyType = dependencyType,
-                Definition = dependency
-            });
-
-            return dependency;
-        }
-
-        public void Child(Type dependencyType, object value)
-        {
-            Dependencies.Add(new ValueDependency
-            {
-                DependencyType = dependencyType,
-                Value = value
-            });
-        }
-
-        public void AcceptVisitor(IDependencyVisitor visitor)
-        {
-            if (Dependencies != null)
-            {
-                Dependencies.Each(x => x.AcceptVisitor(visitor));
-            }
-        }
-
-        public void Child<T>(T value)
-        {
-            Child(typeof(T), value);
-        }
 
         public override string ToString()
         {
-            return string.Format("Name: {0}, Type: {1}, Value: {2}", Name, Type, Value);
+            return string.Format("Name: {0}, Type: {1}", Name, Type);
         }
     }
 }
